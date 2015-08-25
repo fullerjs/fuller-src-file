af517e16a747cb28cc055d7b0d690321266034ea
Alexey Novikov <velocityzen@gmail.com>
2015-08-25 16:35:52 -0400

    Revert "Simplified code"
    This reverts commit 84b3c2d97271ce500b88f96de1ac1c7e64687a2c.

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 "use strict";
+let path = require("path");
+let Transform = require("stream").Transform;
+let Material = require("fuller-material-file");
 
-module.exports = function(f, mat, options, next) {
-	f.addDependencies(mat);
-	next(null, mat);
+let SrcFile = function(fuller, options) {
+	fuller.bind(this);
+
+	this.src = options.src;
+	this.dst = options.dst;
 };
+
+SrcFile.prototype = {
+	build: function(src, dst) {
+		let self = this;
+
+		let next = new Transform({
+			objectMode: true,
+			transform: function(mat, enc, cb) {
+				cb(null, mat);
+			}
+		});
+
+		process.nextTick(function() {
+			let srcfile = path.join(self.src, src),
+				newMat = new Material({
+					id: dst,
+					path: srcfile
+				})
+				.dst(path.join(self.dst, dst))
+				.error(function(err) {
+					self.error(err);
+				});
+
+			self.addDependencies(srcfile, dst);
+			next.write(newMat);
+			next.end();
+		});
+
+		return next;
+	}
+};
+
+
+module.exports = SrcFile;
