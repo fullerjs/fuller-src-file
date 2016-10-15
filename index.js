e049280a7d4c14a83ca79f9be0e9a38de98e7d6c
velocityzen <velocityzen@gmail.com>
2016-10-14 23:57:57 -0400

    + editor config

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-"use strict";
-let path = require("path");
-let Transform = require("stream").Transform;
-let Material = require("fuller-material-file");
+'use strict';
+let path = require('path');
+let Transform = require('stream').Transform;
+let Material = require('fuller-material-file');
 
 let SrcFile = function(fuller, options) {
-	fuller.bind(this);
-
-	this.src = options.src;
-	this.dst = options.dst;
+  fuller.bind(this);
+  this.src = options.src;
+  this.dst = options.dst;
 };
 
 SrcFile.prototype = {
-	build: function(src, dst) {
-		let self = this;
+  build: function(src, dst) {
+    let self = this;
 
-		let next = new Transform({
-			objectMode: true,
-			transform: function(mat, enc, cb) {
-				cb(null, mat);
-			}
-		});
+    let next = new Transform({
+      objectMode: true,
+      transform: function(mat, enc, cb) {
+        cb(null, mat);
+      }
+    });
 
-		process.nextTick(function() {
-			let srcfile = path.join(self.src, src),
-				newMat = new Material({
-					id: dst,
-					path: srcfile
-				})
-				.dst(path.join(self.dst, dst))
-				.error(function(err) {
-					self.error(err);
-				});
+    process.nextTick(function() {
+      let srcfile = path.join(self.src, src);
+      let newMat = new Material({
+        id: dst,
+        path: srcfile
+      })
+      .dst(path.join(self.dst, dst))
+      .error(function(err) {
+        self.error(err);
+      });
 
-			self.addDependencies(srcfile, dst);
-			next.write(newMat);
-			next.end();
-		});
+      self.addDependencies(srcfile, dst);
+      next.write(newMat);
+      next.end();
+    });
 
-		return next;
-	}
+    return next;
+  }
 };
 
 
