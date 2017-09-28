c4abe60b772f8672c84a280c3481d1ceb1aa4b9c
velocityzen <velocityzen@gmail.com>
2017-09-27 22:35:00 -0400

    Updated dependencies

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 'use strict';
-let path = require('path');
-let Transform = require('stream').Transform;
-let Material = require('fuller-material-file');
+const path = require('path');
+const Transform = require('stream').Transform;
+const Material = require('fuller-material-file');
 
-let SrcFile = function(fuller, options) {
+const Tool = function(fuller, options) {
   fuller.bind(this);
   this.src = options.src;
   this.dst = options.dst;
 };
 
-SrcFile.prototype = {
+Tool.prototype = {
   build: function(src, dst) {
-    let self = this;
-
-    let next = new Transform({
+    const next = new Transform({
       objectMode: true,
-      transform: function(mat, enc, cb) {
-        cb(null, mat);
-      }
+      transform: (mat, enc, cb) => cb(null, mat)
     });
 
-    process.nextTick(function() {
-      let srcfile = path.join(self.src, src);
-      let newMat = new Material({
+    process.nextTick(() => {
+      const srcfile = path.join(this.src, src);
+      const newMat = new Material({
         id: dst,
         path: srcfile
       })
-      .dst(path.join(self.dst, dst))
-      .error(function(err) {
-        self.error(err);
-      });
+        .dst(path.join(this.dst, dst))
+        .error(err => this.error(err));
 
-      self.addDependencies(srcfile, dst);
+      this.addDependencies(srcfile, dst);
       next.write(newMat);
       next.end();
     });
@@ -41,4 +35,4 @@ SrcFile.prototype = {
 };
 
 
-module.exports = SrcFile;
+module.exports = Tool;
